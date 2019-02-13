6c84516da016601ea4678edf8849eae5195f5cf7
JPRickabaugh <rickabjp@live.unc.edu>
2019-02-13 01:08:13 +0000

    purple 2/3

--- public/client.js
+++ public/client.js
@@ -50,6 +50,8 @@ $(function() {
     });
   });
   
+  
+  
   $.get('/audio-features', function(data) {
     // "Data" is the object we get from the API. See server.js for the function that returns it.
     console.group('%cResponse from /audio-features', 'color: #F037A5; font-size: large');
